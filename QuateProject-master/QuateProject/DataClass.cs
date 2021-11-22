cd65089ad4179ebab482b03b2d9dbe43a36c2836
daryagrishukova <94660023+daryagrishukova@users.noreply.github.com>
2021-11-22 10:10:25 +0900

    Add files via upload

---
+++ QuateProject-master/QuateProject/DataClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace QuateProject
+{
+    class DataClass
+    {
+        MySqlConnectionStringBuilder connectionStr;
+        MySqlConnection connection;
+
+        // Метод создания Строки подключения, и Коннекшина
+        public void CreateStrConnection()
+        {
+            connectionStr = new MySqlConnectionStringBuilder();
+            connectionStr.Server = "localhost";
+            connectionStr.UserID = "root";
+            connectionStr.Password = "k403";
+            connectionStr.Database = "quote";
+            connection = new MySqlConnection(connectionStr.ToString());
+        }
+
+        public void AddQuote(string Text, string Author, string Link )
+        {
+
+            string CommandText = $"INSERT INTO quotes (Text,Author,Link) VALUES ('{Text}','{Author}','{Link}');";
+
+
+            try
+            {
+
+                connection.Open();
+
+                MySqlCommand command = new MySqlCommand(CommandText, connection);
+
+                command.ExecuteNonQuery();
+
+            }
+            catch (Exception error)
+            {
+                System.Windows.MessageBox.Show(error.Message);
+            }
+            connection.Close();
+
+        }
+        public List<QuotesData> ReadQuote()
+        {
+            List<QuotesData> quotes = new List<QuotesData>();
+
+            string CommandText = $"SELECT * FROM quotes;";
+
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(CommandText, connection);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        quotes.Add(new QuotesData()
+                        {
+                            id = reader.GetInt32(0),
+                            Text = reader.GetString(1),
+                            Author = reader.GetString(2),
+                            Link = reader.GetString(3)
+                        });
+                    }
+                }
+
+            }
+            catch (Exception error)
+            {
+                System.Windows.MessageBox.Show(error.Message);
+            }
+            connection.Close();
+            return quotes;
+        }
+    }
+}
---
+++ QuateProject-master/QuateProject/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace QuateProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClass db = new DataClass();
+        public MainWindow()
+        {
+            InitializeComponent();
+            db.CreateStrConnection();
+        }
+
+        private void btnGetQuate_Click(object sender, RoutedEventArgs e)
+        {
+            // Объявляем экземпляр класса Цитат
+            Quote qoute = new Quote();
+
+            // Отправляем запрос для получения случайной цитаты
+            // И сохраняем полученный результат в наш ранее созданный объект
+            qoute = qoute.GetQuote();
+
+            // Выводим поле текст цитаты в label на форме
+            lblQuate.Text = qoute.quoteText;
+            lblAutor.Text = qoute.quoteAuthor;
+            if (String.IsNullOrEmpty(lblAutor.Text))
+                lblAutor.Text = "Нет автора";
+            lblLink.Text = qoute.quoteLink;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Best best = new Best();
+            best.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            db.AddQuote(lblQuate.Text, lblAutor.Text, lblLink.Text);
+        }
+    }
+    
+}
